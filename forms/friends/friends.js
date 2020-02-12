8fd9cb91f2b80a96f193e1c39357fd3f7ebcf013
pvaupel <pov46864@creighton.edu>
2020-02-12 13:34:05 -0600

    test

--- forms/friends/friends.js
+++ forms/friends/friends.js
@@ -1,21 +1,45 @@
-/*
-prompt("Enter the name of your first of three friends.");
-Prompt("Enter the name of your second of three friends.");
-prompt("Enter the name of your third of three friends.");
-alert("Enter a 1 if your first person was your best friend, a 2 for the second, and a 3 for the third")
+//recieves the friend names from user, holding them in variables
+let firstFriend = prompt("Enter the name of your first of three friends.")
+let secondFriend = prompt("Enter the name of your second of three friends.")
+let thirdFriend = prompt("Enter the name of your third of three friends.")
 
-var friendNames = ();
+//recieves which friend was the best friend from the user storing information as a number
+let bestFriend =  Number(prompt('Enter a 1 if your first person was your best friend, a 2 for the second, and a 3 for the third.'))
 
-friendNames[1] = firstFriend
-friendNames[2] = secondFriend
-friendNames[3] = thirdFriend
-let bestFriend = friendNames[best - 1]
-
-if (bestFriend.firstChar() == "A")
-   alert("Your best friends' name starts with A.")
-else if (bestFriend.firstChar() = "B")
-   alert("Your best friends' name starts with B.")
-else 
-   alert("Your name starts with something other than A or B.")
-
-
+//creates a variable to hold array of friends names and displays them
+var friendNames = [`${firstFriend}`,`${secondFriend}`,`${thirdFriend}`]
+  alert(friendNames[0])
+  alert(friendNames[1])
+  alert(friendNames[2])
+  
+//deciphers the value stored in bestfriend variable, relating it to a friend variable
+//alerts which friend is your best friend
+//Decides whether the name starts with an A or a B and displays an appropriate message
+if (bestFriend == 1) {
+   alert(`Your best friend is: ${friendNames[0]}`);
+       if (friendNames[0].charAt(0) == 'A' || friendNames[0].charAt(0) == 'a') {
+       alert(`Your best friends name starts with A.`);
+       } else if (friendNames[0].charAt(0) == 'B' || friendNames[0].charAt(0) == 'b') {
+       alert(`Your best friends name starts with B.`);
+       } else {
+       alert(`Your best friends name starts with something other than A or B.`);
+       }
+  } else if (bestFriend == 2) {
+   alert(`Your best friend is:  ${friendNames[1]}`);
+       if (friendNames[1].charAt(0) == 'A' || friendNames[1].charAt(0) == 'a') {
+       alert(`Your best friends name starts with A.`);
+       } else if (friendNames[1].charAt(0) == 'B' || friendNames[1].charAt(0) == 'b') {
+       alert(`Your best friends name starts with B.`);
+       } else {
+       alert(`Your best friends name starts with something other than A or B.`);
+       }
+  } else if (bestFriend == 3) {
+   alert(`Your best friend is:  ${friendNames[2]}`);
+        if (friendNames[2].charAt(0) == 'A' || friendNames[2].charAt(0) == 'a') {
+       alert(`Your best friends name starts with A.`);
+       } else if (friendNames[2].charAt(0) == 'B' || friendNames[2].charAt(0) == 'b') {
+       alert(`Your best friends name starts with B.`);
+       } else {
+       alert(`Your best friends name starts with something other than A or B.`);
+       }
+ } 
